e31c0ae149becc050bd476a2b3f9537328eb9a9d
guomw <516985724@163.com>
2017-03-08 21:49:26 +0800

    ceshi

--- HotWeChatUtils/Form1.Designer.cs
+++ HotWeChatUtils/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +43,41 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(248, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 259);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- HotWeChatUtils/Form1.cs
+++ HotWeChatUtils/Form1.cs
@@ -24,26 +24,32 @@ namespace HotWeChatUtils
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
+
+
+
+
+
+
+
+
+
+
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             string str = "[商品标题]\n[分隔符]\n【原价】：[商品价格]元\n【券后】：[券后价格]元\n【口令】：[二合一淘口令]\n[分隔符]\n购买方式：复制这条信息，打开『手" +
       "机淘宝』即可看到商品和优惠券，先领券再下单哦\n[分隔符]\n本群都是内部优惠券，敬请大家关注每天特价产品。\n";
+            Clipboard.Clear();
             Clipboard.SetText(str);
             IntPtr hWin = WinApi.GetWeChatWindowEx();
-            //var wins = WinApi.GetAllDesktopWindows();
-            //if (wins != null && wins.Count() > 0)
-            //{
-            //    foreach (var win in wins)
-            //    {
-            //        WinApi.ShowWindow(hWin, WinApi.NCmdShowFlag.SW_SHOWNORMAL);
-            //        Clipboard.SetText("hello world 中文");
-            //        SendKeys.SendWait("^v");
-            //        //SendKeys.SendWait("{ENTER}");
-            //        //WinApi.InputStr(win.hWnd, "hello world 中午");
-            //    }
-            //}
             if (hWin != IntPtr.Zero)
             {
+                var dd = WinApi.FindWindowEx(hWin, IntPtr.Zero, "EditWnd", null);
+
                 WinApi.ShowWindow(hWin, WinApi.NCmdShowFlag.SW_SHOWNORMAL);
                 RECT rc = new RECT();
                 WinApi.GetWindowRect(hWin, ref rc);
@@ -51,16 +57,22 @@ namespace HotWeChatUtils
                 int y = rc.Top; // Y coordinate of the click 
                 SetForegroundWindow(hWin);
 
-                IntPtr lParam =(IntPtr)((y << 534) | x); // The coordinates                
+                IntPtr lParam = (IntPtr)((y << 16) | x); // The coordinates                
                 IntPtr wParam = IntPtr.Zero; // Additional parameters for the click (e.g. Ctrl) 
                 const uint downCode = 0x201; // Left click down code 
                 const uint upCode = 0x202; // Left click up code 
-                System.Threading.Thread.Sleep(3000);
-                SendMessage(hWin, downCode, wParam, lParam); // Mouse button down 
-                SendMessage(hWin, upCode, wParam, lParam); // Mouse button up 
+                                           //System.Threading.Thread.Sleep(3000);
 
-                        
-                
+
+                WinApi.SetCursorPos(x + 20, y + 400);
+                int WM_SETHOTKEY = 0x0032;
+                int ctrldd = 0x02;
+                //WinApi.mouse_event(WinApi.MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+                //WinApi.mouse_event(WinApi.MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+
+                SendMessage(hWin, downCode, wParam, lParam); // Mouse button down 
+                //System.Threading.Thread.Sleep(3000);
+                SendMessage(hWin, upCode, wParam, lParam); // Mouse button up 
                 //WinApi.InputStr(hWin, str);
                 //SendMessage(hWin, WM_SETTEXT, IntPtr.Zero, new StringBuilder("Hello World! 中文"));
                 //WinApi.InputStr(hWin, "hello world 中午");
@@ -68,6 +80,33 @@ namespace HotWeChatUtils
                 //回车
                 //WinApi.keybd_event(Keys.Enter, 0, 0, 0);
             }
+
+            timer.Interval = 5000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        Timer timer = new Timer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetFocus();
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Point defPnt = new Point();
+            // Call the function and pass the Point, defPnt
+            GetCursorPos(ref defPnt);
+            IntPtr d = GetFocus();// WinApi.WindowFromPoint(defPnt);
+            StringBuilder sb = new StringBuilder(256);
+            WinApi.GetClassName(d, sb, sb.Capacity);
+            textBox1.Text = sb.ToString();
+
+            WinApi.GetWindowText(d, sb, sb.Capacity);
+
+            textBox2.Text = sb.ToString();
         }
     }
 }
